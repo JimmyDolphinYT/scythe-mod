b0e8abd1b40642b170f9e8af5bac756d6b41749a
Torchwood-1 <jacobroberts@virginmedia.com>
2022-12-26 21:53:18 +0000

    changed variable names

--- src/main/java/com/jdolphin/scythe/item/ScytheItem.java
+++ src/main/java/com/jdolphin/scythe/item/ScytheItem.java
@@ -8,8 +8,8 @@ import net.minecraftforge.common.ToolActions;
 
 public class ScytheItem extends AxeItem {
 
-    public ScytheItem(Tier p_40521_, float p_40522_, float p_40523_, Properties p_40524_) {
-        super(p_40521_, p_40522_, p_40523_, p_40524_);
+    public ScytheItem(Tier tier, float v, float v1, Properties properties) {
+        super(tier, v, v1, properties);
     }
 
     @Override
