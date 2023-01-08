3047edf5e1c91fad49448e14c18b0a51fd438f4e
SamTees <26529248+SamTees@users.noreply.github.com>
2023-01-08 16:34:23 +0000

    Add keybinds for ScytheAbilityScreen

--- src/main/java/com/jdolphin/scythe/client/gui/screens/ScytheAbilityScreen.java
+++ src/main/java/com/jdolphin/scythe/client/gui/screens/ScytheAbilityScreen.java
@@ -2,12 +2,15 @@ package com.jdolphin.scythe.client.gui.screens;
 
 import com.google.common.collect.Lists;
 import com.jdolphin.scythe.ScytheMod;
+import com.jdolphin.scythe.keybind.Keybinds;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.toasts.SystemToast;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
@@ -18,58 +21,77 @@ import java.util.List;
 
 @OnlyIn(Dist.CLIENT)
 public class ScytheAbilityScreen extends Screen {
-    private static final ResourceLocation BACKGROUND_TEXTURE = new ResourceLocation(ScytheMod.MOD_ID, "textures/gui/clockwork_gui.png");
-    int backgroundWidth = 256;
-    int backgroundHeight = 256;
-    private final List<Button> abilityButtons = Lists.newArrayList();
+	private static final ResourceLocation BACKGROUND_TEXTURE = new ResourceLocation(ScytheMod.MOD_ID, "textures/gui/clockwork_gui.png");
+	int backgroundWidth = 256;
+	int backgroundHeight = 256;
+	private final List<Button> abilityButtons = Lists.newArrayList();
 
-    public ScytheAbilityScreen() {
-        super(new TranslatableComponent("menu.scythe.ability"));
-    }
+	protected final KeyMapping[] keyMappings = new KeyMapping[]{
+			Keybinds.KEY_SCYTHE_MENU_DIMENSION_HOP,
+			Keybinds.KEY_SCYTHE_MENU_DASH,
+			Keybinds.KEY_SCYTHE_MENU_INVISIBILITY,
+			Keybinds.KEY_SCYTHE_MENU_PASSIVE
+	};
 
-    @Override
-    protected void init() {
-        this.abilityButtons.add(this.addRenderableWidget(new Button(this.width / 2 - 45, this.height / 4 - 23, 85, 20,
-                new TranslatableComponent("scythe.ability.dim_hop"), (p_95930_) -> {
-            Object ScytheDimensionScreen = new ScytheDimensionScreen();
-            this.minecraft.setScreen((Screen)ScytheDimensionScreen);
+	public ScytheAbilityScreen() {
+		super(new TranslatableComponent("menu.scythe.ability"));
+	}
 
-        })));
-        this.abilityButtons.add(this.addRenderableWidget(new Button(this.width / 2 - 150, this.height / 2 - 10, 85, 20,
-                new TranslatableComponent("scythe.ability.dash"), (p_95930_) -> {
+	@Override
+	protected void init() {
+		this.abilityButtons.add(this.addRenderableWidget(new Button(this.width / 2 - 45, this.height / 4 - 23, 85, 20,
+				new TranslatableComponent("scythe.ability.dim_hop"), (p_95930_) -> {
+			Object ScytheDimensionScreen = new ScytheDimensionScreen();
+			this.minecraft.setScreen((Screen) ScytheDimensionScreen);
+		})));
+		this.abilityButtons.add(this.addRenderableWidget(new Button(this.width / 2 - 150, this.height / 2 - 10, 85, 20,
+				new TranslatableComponent("scythe.ability.dash"), (p_95930_) -> {
+			this.minecraft.getToasts().addToast(new SystemToast(SystemToast.SystemToastIds.TUTORIAL_HINT, new TextComponent("Dash"), null));
+		})));
+		this.abilityButtons.add(this.addRenderableWidget(new Button(this.width / 2 - 45, this.height / 2 + 90, 85, 20,
+				new TranslatableComponent("scythe.ability.invisibility"), (p_95930_) -> {
+			this.minecraft.getToasts().addToast(new SystemToast(SystemToast.SystemToastIds.TUTORIAL_HINT, new TextComponent("Invisible"), null));
+		})));
+		this.abilityButtons.add(this.addRenderableWidget(new Button(this.width / 2 + 55, this.height / 2 - 10, 85, 20,
+				new TranslatableComponent("scythe.ability.passive"), (p_95930_) -> {
+			this.minecraft.getToasts().addToast(new SystemToast(SystemToast.SystemToastIds.TUTORIAL_HINT, new TextComponent("Passive"), null));
+		})));
+		super.init();
+	}
 
-        })));
-        this.abilityButtons.add(this.addRenderableWidget(new Button(this.width / 2 - 45, this.height / 2 + 90, 85, 20,
-                new TranslatableComponent("scythe.ability.invisibility"), (p_95930_) -> {
+	@Override
+	public boolean keyPressed(int pKeyCode, int pScanCode, int pModifiers) {
+		for (int i = 0; i < keyMappings.length; i++) {
+			if (keyMappings[i].matches(pKeyCode, pScanCode)) {
+				this.abilityButtons.get(i).onPress();
+				break;
+			}
+		}
 
-        })));
-        this.abilityButtons.add(this.addRenderableWidget(new Button(this.width / 2 + 55, this.height / 2 - 10, 85, 20,
-                new TranslatableComponent("scythe.ability.passive"), (p_95930_) -> {
+		return super.keyPressed(pKeyCode, pScanCode, pModifiers);
+	}
 
-        })));
-        super.init();
-    }
+	@Override
+	public void render(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
+		this.renderBackground(pPoseStack);
+		renderBackground(pPoseStack, 1, 1, 1);
+		super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
+	}
 
-    @Override
-    public void render(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
-        this.renderBackground(pPoseStack);
-        renderBackground(pPoseStack, 1, 1, 1);
-        super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
-    }
+	protected void renderBackground(PoseStack pPoseStack, float pPartialTick, int pMouseX, int pMouseY) {
+		GL11.glEnable(GL11.GL_BLEND);
 
-    protected void renderBackground(PoseStack pPoseStack, float pPartialTick, int pMouseX, int pMouseY) {
-        GL11.glEnable(GL11.GL_BLEND);
+		RenderSystem.setShader(GameRenderer::getPositionTexShader);
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+		RenderSystem.setShaderTexture(0, BACKGROUND_TEXTURE);
+		int x = (width - backgroundWidth) / 2 + 2;
+		int y = (height - backgroundHeight) / 2 + 2;
 
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, BACKGROUND_TEXTURE);
-        int x = (width - backgroundWidth) / 2 + 2;
-        int y = (height - backgroundHeight) / 2 + 2;
+		this.blit(pPoseStack, x, y, 0, 0, backgroundWidth, backgroundHeight);
+	}
 
-        this.blit(pPoseStack, x, y, 0, 0, backgroundWidth, backgroundHeight);
-    }
-    public boolean isPauseScreen() {
-        return false;
-    }
+	public boolean isPauseScreen() {
+		return false;
+	}
 
 }
--- src/main/java/com/jdolphin/scythe/keybind/Keybinds.java
+++ src/main/java/com/jdolphin/scythe/keybind/Keybinds.java
@@ -13,9 +13,25 @@ public class Keybinds {
 
     public static final String SCYTHE_MENU_CATEGORY = "key.category.scythe";
     public static final String SCYTHE_MENU_KEY = "key.scythe.scythe_menu";
+    public static final String SCYTHE_MENU_DIMENSION_HOP_KEY = "key.scythe.scythe_menu.dimension_hop";
+    public static final String SCYTHE_MENU_DASH_KEY = "key.scythe.scythe_menu.dash";
+    public static final String SCYTHE_MENU_PASSIVE_KEY = "key.scythe.scythe_menu.passive";
+    public static final String SCYTHE_MENU_INVISIBILITY_KEY = "key.scythe.scythe_menu.invisibility";
 
     public static final KeyMapping KEY_SCYTHE_MENU = createMapping(SCYTHE_MENU_KEY, KeyConflictContext.IN_GAME,
             InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_V, SCYTHE_MENU_CATEGORY);
+
+    public static final KeyMapping KEY_SCYTHE_MENU_DIMENSION_HOP = createMapping(SCYTHE_MENU_DIMENSION_HOP_KEY, KeyConflictContext.GUI,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_W, SCYTHE_MENU_CATEGORY);
+
+    public static final KeyMapping KEY_SCYTHE_MENU_DASH = createMapping(SCYTHE_MENU_DASH_KEY, KeyConflictContext.GUI,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_A, SCYTHE_MENU_CATEGORY);
+
+    public static final KeyMapping KEY_SCYTHE_MENU_INVISIBILITY = createMapping(SCYTHE_MENU_INVISIBILITY_KEY, KeyConflictContext.GUI,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_S, SCYTHE_MENU_CATEGORY);
+
+    public static final KeyMapping KEY_SCYTHE_MENU_PASSIVE = createMapping(SCYTHE_MENU_PASSIVE_KEY, KeyConflictContext.GUI,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_D, SCYTHE_MENU_CATEGORY);
 
     protected static KeyMapping createMapping(String name, KeyConflictContext context, InputConstants.Type type, int binding, String category) {
         KeyMapping mapping = new KeyMapping(name, context, type, binding, category);
